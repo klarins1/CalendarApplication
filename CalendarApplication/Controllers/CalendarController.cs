8f0dcfec4738a8dcba0720dfc9e47da70bd0cb1d
klarins1 <clarencetilaon.69@gmail.com>
2026-06-07 22:27:53 +0800

    Initial commit

---
+++ CalendarApplication/Controllers/CalendarController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using CalendarBL;
+using CalendarModels;
+
+namespace CalendarApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalendarController : ControllerBase
+    {
+        private readonly CalendarAppService _appservice;
+
+        public CalendarController()
+        {
+            _appservice = new CalendarAppService();
+        }
+
+        [HttpGet]
+        public ActionResult<List<EventModels>> Get()
+        {
+                var events = _appservice.GetEventModels();
+                return Ok(events);
+        }
+
+        [HttpGet("{name}")]
+        public ActionResult<EventModels> Get(string name)
+        {
+            var eventModel = _appservice.GetEventModels(name);
+            if (eventModel == null)
+            {
+                return NotFound();
+            }
+            return Ok(eventModel);
+        }
+
+        [HttpPost]
+        public ActionResult Add(EventModels eventModel)
+        {
+            _appservice.AddEvent(eventModel.name, eventModel.date);
+            return CreatedAtAction(nameof(Get), new { name = eventModel.name }, eventModel);
+        }
+    }
+}   
