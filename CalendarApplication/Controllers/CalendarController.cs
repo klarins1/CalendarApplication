68cae8ccc8077f07171c65608355b1de39dfb499
klarins1 <clarencetilaon.69@gmail.com>
2026-06-07 23:27:54 +0800

    Initial Commit

--- CalendarApplication/Controllers/CalendarController.cs
+++ CalendarApplication/Controllers/CalendarController.cs
@@ -42,5 +42,32 @@ namespace CalendarApplication.Controllers
             _appservice.AddEvent(eventModel.name, eventModel.date);
             return CreatedAtAction(nameof(Get), new { name = eventModel.name }, eventModel);
         }
+
+        [HttpPut]
+        public ActionResult Update([FromBody] EventModels updatedModel)
+        {
+            var existingEvent = _appservice.GetEventModels(updatedModel.name);
+            if (existingEvent == null)
+            {
+                return NotFound(new { message = $"Event '{updatedModel.name}' " });
+            }
+
+            _appservice.UpdateEvent(updatedModel.name, updatedModel);
+            return Ok(new { message = $"Successfully updated!", data = updatedModel });
+        }
+
+        [HttpDelete("{name}")]
+        public ActionResult DeleteEvent(string name)
+        {
+            var existingEvent = _appservice.GetEventModels(name);
+            if (existingEvent == null)
+            {
+                return NotFound(new { message = $"Event '{name}' ay hindi nahanap sa database." });
+            }
+
+            _appservice.DeleteEvent(name);
+            return Ok(new { message = $"Event '{name}' successfully deleted!" });
+        }
+
     }
 }   
